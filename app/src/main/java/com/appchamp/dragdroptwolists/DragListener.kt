67e8ebc052c329c1754c04f9acab7be9e8dfa326
trandat <trandat728@gmail.com>
2023-10-28 10:50:42 +0700

    drag drop

--- app/src/main/java/com/appchamp/dragdroptwolists/DragListener.kt
+++ app/src/main/java/com/appchamp/dragdroptwolists/DragListener.kt
@@ -10,8 +10,30 @@ import androidx.recyclerview.widget.RecyclerView
  * khi đó ta cần làm là có thể drag được view chứa fragment
  */
 
-/**
- *
+/** ý tưởng khi drop :
+ * View pager chứa nhiều page, 1 page có 2 fragment
+ * ở trái, phải fragment sẽ có 1 view nhỏ, khi drag tới sẽ thay đổi size và đổi màu
+ * v = view, F = Fragment, 0 = rỗng, E = EmptyView
+ * vFvFv : f1 + f2
+ * 0E0Fv : E + f2
+ * vF0E0 : f1 + E
+
+ * Th1 : Khi kéo Fragment vào vị trí 1
+ *      vFvFv -> vFvFv vFvEv
+ *      0E0Fv -> vFvFv
+ *      vF0E0 -> vFvFv
+ * Th2 : Khi kéo fragment vào vị trí 2
+ *      vFvFv -> vFvFv vFvEv
+ *      0E0Fv -> 0E0Fv (Không có vị trí 2, ở giữa E và F và view rỗng)
+ *      vF0E0 -> vF0E0 (Không có vị trí 2, ở giữa E và F và view rỗng)
+ * Th3 : Khi kéo fragment vào vị trí 3
+ *      vFvFv -> Khi này view sẽ tìm vị trí trống ở các page trước nó
+ *               có -> Xoá view trống và đẩy các Fragment về trước 1 ô
+ *               không -> Không biết ???????
+ *      0E0Fv -> vFvFv
+ *      vF0E0 -> vFvFv
+ * => Xử lý ở viewpager có những trường hợp cần đẩy các fragment ra sau hoặc ra trước, chạy
+animation để view giữa các viewpager (Cái này có thể sử dụng fade mà không dùng slide anim )
  */
 class DragListener(val onViewEntered: (View) -> Unit, val onViewExited: (View) -> Unit) :
     View.OnDragListener {
@@ -38,12 +60,12 @@ class DragListener(val onViewEntered: (View) -> Unit, val onViewExited: (View) -
             }
 
             DragEvent.ACTION_DRAG_ENDED -> {
-//                val view = event.localState as? View?
-//                if (!isDropped) {
-//                    view?.post {
-//                        view.visibility = View.VISIBLE
-//                    }
-//                }
+                val view = event.localState as? View?
+                if (!isDropped) {
+                    view?.post {
+                        view.visibility = View.VISIBLE
+                    }
+                }
             }
 
             DragEvent.ACTION_DROP -> {
